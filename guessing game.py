acc57bee3f0b6b77aabefb69ed7698c10ebdd91b
Elvistee <76506721+Elvistee1@users.noreply.github.com>
2021-02-27 11:11:57 +0200

    Add files via upload

---
+++ guessing game.py
@@ -0,0 +1,19 @@
+__author__ = 'Thabo'
+
+secret_word = "banana"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guess = False
+
+while guess != secret_word and not(out_of_guess):
+    if guess_count < guess_limit:
+        guess = input("Enter a guess : ")
+        guess_count += 1
+    else:
+        out_of_guess = True
+
+if out_of_guess :
+    print("You lose !!")
+else:
+    print("You win!!")
